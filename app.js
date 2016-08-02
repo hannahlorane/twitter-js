49f1d939917e84e29aee6ad936e89d1ca6c15b58
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-08-02 16:01:12 -0400

    swig is ambiently irritating

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require("express");
+var swig = require('swig');
+
 var app = express();
+app.listen(3000);
+
+swig.setDefaults({cache: false});
+app.engine('html', swig.renderFile);
+app.set("views", "/views");
+app.set("view engine", "html");
 
 console.log("server listening");
-
-app.listen(3000);
 
 app.all("*", function (req, res, next) {
   console.log(req.method + ": " + req.path);
@@ -21,5 +27,8 @@ app.get("/news", function (req, res) {
 
 
 app.get("/*", function (req, res) {
-  res.send("You are on a thing! good job!");
+  res.render("index",  {
+    title : "This is our awesome app",
+    people: [{name: "Horkheimer"}, {name: "Adorno: Jazz-Hater"}, {name: "Benjamin the Magickal"}],
+  });
 });
---
+++ swing-render.js
@@ -0,0 +1,2 @@
+//swig = require("swig");
+//swig.renderFile("views/index.html");
