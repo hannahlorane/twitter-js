4b18f03fdd09db05516e2bfa9717e72991bd85ec
Hannah Katznelson <hannah.katznelson@gmail.com>
2016-08-02 15:25:08 -0400

    commit early commit often

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ var app = express();
 console.log("server listening");
 
 app.listen(3000);
+
+app.all("*", function (req, res, next) {
+  console.log(req.method + ": " + req.path);
+  next();
+});
+
+app.get("/unreal", function (req, res, next) {
+  console.log("This is what a feminist looks like");
+  next();
+});
+
+app.get("/news", function (req, res) {
+  res.send("we create magic filepaths that are not filepaths!!!");
+});
+
+
+app.get("/*", function (req, res) {
+  res.send("You are on a thing! good job!");
+});
